5aba8038f57accdab6ada76ffb613cf63e682a94
agung5114 <43102810+agung5114@users.noreply.github.com>
2021-06-04 07:43:19 +0700

    Update st_nlp.py

--- st_nlp.py
+++ st_nlp.py
@@ -5,37 +5,25 @@ import pandas as pd
 import numpy as np
 from wordcloud import WordCloud
 import json
+import pickle 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.decomposition import TruncatedSVD
 
-def nlp_predict(base,text):
-    v = TfidfVectorizer()
-    x = v.fit_transform(base)
-    clf = TruncatedSVD(3)
-    Xpca = clf.fit_transform(x)
-    kmeans = KMeans(n_clusters=10).fit(Xpca)
+def nlp_predict(text, v, clf, kmeans):
     text_tfidf = v.transform([text])
     text_pca = clf.transform(text_tfidf)
     hasil = kmeans.predict(text_pca)
     return hasil[0]
 
-def nlp_plot(dfbase):
-    dfbase['data'] = dfbase['data'].astype('str')
-    base = dfbase['data']
-    v = TfidfVectorizer()
-    x = v.fit_transform(base)
-    clf = TruncatedSVD(3)
-    Xpca = clf.fit_transform(x)
-    return Xpca
-
-# Load dataset
-df = pd.read_csv('nlpbase.csv')
-df['data'] = df['data'].astype('str')
-dfbase = df['data']
 dfwc = pd.read_csv('final10.csv')
+# dfwc = pickle.load(open('dfwc.pkl', 'rb'))
+Xpca = pickle.load(open('xpca.pkl', 'rb'))
+kmeans = pickle.load(open('kmeans.pkl', 'rb'))
+v = pickle.load(open('v.pkl', 'rb'))
+clf = pickle.load(open('clf.pkl', 'rb'))
 
 st.set_page_config(layout="wide")
 t1, t2 = st.beta_columns((1,2))
@@ -43,24 +31,23 @@ with t1:
     input_text = st.text_input(label="Kata Kunci",value="")
     output = None
     if st.button("Submit"):
-        output = nlp_predict(dfbase,input_text)
+        output = nlp_predict(input_text, v, clf, kmeans)
     st.write(f'Klaster Hasil Prediksi NLP: {output}')   
 with t2:
     data = dfwc
     if output == None:
-        Xpca = nlp_plot(dfwc)
+        # Xpca = nlp_plot(dfwc)
         fig0=px.scatter_3d(data, x=Xpca[:, 0], y=Xpca[:, 1], z=Xpca[:, 2]
                     ,color= 'label',title='Klaster Anggaran')
-        fig0.update_layout(height=500)
     else:
         output = int(output)
         data = data[data['label'].isin([output])]
         cek = data['data']
         wordcloud = WordCloud (
                     background_color = 'white',
-                    width = 800,
+                    width = 512,
                     stopwords =['belanja','pendapatan','lra','transfer','dan','kepada','yang'],
-                    height = 500
+                    height = 350
                         ).generate(' '.join(cek))
         fig0 = px.imshow(wordcloud,title=f'Wordcloud Klaster Anggaran {output}')
     st.plotly_chart(fig0)
