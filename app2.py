052cc75d8febbff7e20e98d91d4a95dede38953e
agung5114 <43102810+agung5114@users.noreply.github.com>
2021-07-22 15:15:21 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -11,14 +11,8 @@ from datetime import datetime
 # Utils
 import joblib 
 pipe_lr = joblib.load(open("modelnlp.pkl","rb"))
+pipe_ctm = joblib.load(open("model_custom.pkl","rb"))
 
-# Track Utils
-# from track_utils import create_page_visited_table,add_page_visited_details,view_all_page_visited_details,add_prediction_details,view_all_prediction_details,create_emotionclf_table
-
-# # Googletrans unofficial
-# from googletrans import Translator, constants
-# from pprint import pprint
-# translator = Translator(service_urls=["translate.google.co.id","translate.google.com"])
 
 import tweepy
 # api key
@@ -43,54 +37,66 @@ def get_prediction_proba(docx):
 	results = pipe_lr.predict_proba([docx])
 	return results
 
-emotions_emoji_dict = {"anger":"😠","disgust":"🤮", "fear":"😨😱", "happy":"🤗", "joy":"😂", "neutral":"😐", "sad":"😔", "sadness":"😔", "shame":"😳", "surprise":"😮"}
+def predict_sentiment(docx):
+    results = pipe_ctm.predict([docx])
+    return results[0]
 
+def get_sentiment_proba(docx):
+	results = pipe_ctm.predict_proba([docx])
+	return results
+
+emotions_emoji_dict = {"anger":"😠","disgust":"🤮", "fear":"😨😱", "happy":"🤗", "joy":"😂", "neutral":"😐", "sad":"😔", "sadness":"😔", "shame":"😳", "surprise":"😮"}
+emoji_sentiment = {"positif":"🤗","negatif":"😔","netral":"😐","tdk-relevan":"😮"}
 
 # Main Application
-# def main():
-st.title("Emotion Classifier App")
-menu = ["Home","NER"]
-choice = st.sidebar.selectbox("Menu",menu)
-# create_page_visited_table()
-# create_emotionclf_table()
-if choice == "Home":
-	# add_page_visited_details("Home",datetime.now())
-	st.subheader("Home-Emotion In Text")
+def main():
+    st.title("Emotion Classifier App")
+    menu = ["Home"]
+    choice = st.sidebar.selectbox("Menu",menu)
+    # create_page_visited_table()
+    # create_emotionclf_table()
+    if choice == "Home":
+        # add_page_visited_details("Home",datetime.now())
+        st.subheader("Home-Emotion In Text")
 
-	with st.form(key='emotion_clf_form'):
-		search_text = st.text_area("Type Here")
-		submit_text = st.form_submit_button(label='Submit')
+        with st.form(key='emotion_clf_form'):
+            search_text = st.text_area("Type Here")
+            submit_text = st.form_submit_button(label='Submit')
 
-	if submit_text:	
-		hasilSearch = api.search(q=str(search_text),count=1)
-		raw_text = []
-		for tweet in hasilSearch:
-			raw_text.append(tweet.text)
-		raw_text = raw_text[0]
-		col1,col2  = st.beta_columns(2)
-		# translated = translator.translate(raw_text)
-		# translated = raw_text
-		prediction = predict_emotions(raw_text)
-		probability = get_prediction_proba(raw_text)
-		
-		# add_prediction_details(raw_text,prediction,np.max(probability),datetime.now())
+        if submit_text:	
+            hasilSearch = api.search(q=str(search_text),count=1)
+            raw_text = []
+            for tweet in hasilSearch:
+                raw_text.append(tweet.text)
+            raw_text = raw_text[0]
+            # translated = translator.translate(raw_text)
+            # translated = raw_text
+            prediction = predict_emotions(raw_text)
+            probability = get_prediction_proba(raw_text)
+            sentiment = predict_sentiment(raw_text)
+            proba_sentiment = get_sentiment_proba(raw_text)
+            col1,col2  = st.beta_columns(2)
+            with col1:
+                st.success("Search Result")
+                st.write(raw_text)
+                # st.write(translated.text)
+                st.success("Prediction")
+                emoji_icon = emotions_emoji_dict[prediction]
+                st.write("{}:{}".format(prediction,emoji_icon))
+                st.write("Confidence:{}".format(np.max(probability)))
+                st.write("{}:{}".format(sentiment,emoji_sentiment[sentiment]))
+                st.write("Confidence:{}".format(np.max(proba_sentiment)))
+            with col2:
+                st.success("Prediction Probability")
+                # st.write(probability)
+                proba_df = pd.DataFrame(probability,columns=pipe_lr.classes_)
+                # st.write(proba_df.T)
+                proba_df_clean = proba_df.T.reset_index()
+                proba_df_clean.columns = ["emotions","probability"]
 
-		with col1:
-			st.success("Search Result")
-			st.write(raw_text)
-			# st.write(translated.text)
-			st.success("Prediction")
-			emoji_icon = emotions_emoji_dict[prediction]
-			st.write("{}:{}".format(prediction,emoji_icon))
-			st.write("Confidence:{}".format(np.max(probability)))
+                fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions',y='probability',color='emotions')
+                st.altair_chart(fig,use_container_width=True)
 
-		with col2:
-			st.success("Prediction Probability")
-			# st.write(probability)
-			proba_df = pd.DataFrame(probability,columns=pipe_lr.classes_)
-			# st.write(proba_df.T)
-			proba_df_clean = proba_df.T.reset_index()
-			proba_df_clean.columns = ["emotions","probability"]
 
-			fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions',y='probability',color='emotions')
-			st.altair_chart(fig,use_container_width=True)
+if __name__ == '__main__':
+	main()
