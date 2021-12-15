f6c697c4dddcbda112877d1ef85747d2afa0dc97
agung5114 <43102810+agung5114@users.noreply.github.com>
2021-12-15 11:59:58 +0700

    Update alona2.py

--- alona2.py
+++ alona2.py
@@ -218,12 +218,17 @@ elif choice == 'Anomali':
         if st.checkbox('Correlation Matrix'):
             dfcor2 = df[['Base_IPM','Base_AHH','Base_HLS','Base_RLS','Base_PPK']]
             dfcor1 = df[["Ekonomi","Kesehatan","Ketertiban","Lingkungan","ParBud","Pelayanan","Pendidikan","Sosial","Rumah_Fasum"]]
+            dfall = df[['Base_IPM','Base_AHH','Base_HLS','Base_RLS','Base_PPK',"Ekonomi","Kesehatan","Ketertiban","Lingkungan","ParBud","Pelayanan","Pendidikan","Sosial","Rumah_Fasum"]]
+            st.write(sns.heatmap(dfall.corr(),annot=True,annot_kws={"size": 4}))
+            st.pyplot()
             z1, z2 = st.beta_columns((1,1))
             with z1:
-                st.write(sns.heatmap(dfcor1.corr(),annot=True,annot_kws={"size": 3}))
+                st.write("Correlation Matrix Fungsi")
+                st.write(sns.heatmap(dfcor1.corr(),annot=True,annot_kws={"size": 4}))
                 st.pyplot()
             with z2:
-                st.write(sns.heatmap(dfcor2.corr(),annot=True,annot_kws={"size": 3}))
+                st.write("Correlation Matrix Fungsi")
+                st.write(sns.heatmap(dfcor2.corr(),annot=True,annot_kws={"size": 4}))
                 st.pyplot()
         plot_type = st.selectbox('Select Type of Plot',["bar","line","area","hist","box"])
         all_columns = ["Total","Ekonomi","Kesehatan","Ketertiban","Lingkungan","ParBud","Pelayanan","Pendidikan","Sosial","Rumah_Fasum","Populasi"]
